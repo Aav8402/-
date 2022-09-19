07a6bc14dd469db222b06aacae52614f2db3e7f4
Alexey <aav8402@mail.ru>
2022-09-19 22:33:54 +0300

    Проверка на четность

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая на вход принимает 1 число и выдает, 
+// является ли число четным
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+if (A % 2 == 0)
+{
+Console.WriteLine("A - четное число");
+}
+ else
+{
+Console.WriteLine("A - нечетное число");
+}
+ 
